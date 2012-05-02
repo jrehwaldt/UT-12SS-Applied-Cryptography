8591d248adf20252882c81338a5db0a4bca0913f
jan.rehwaldt <jan.rehwaldt@student.hpi.uni-potsdam.de>
2012-05-02 16:47:29 +0300

    * homework 2-2

--- src/main/java/hw2/OpenpgpUtil.java
+++ src/main/java/hw2/OpenpgpUtil.java
@@ -1,10 +1,19 @@
 package hw2;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Iterator;
 
+import org.bouncycastle.bcpg.PublicKeyAlgorithmTags;
 import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPPublicKey;
 import org.bouncycastle.openpgp.PGPPublicKeyRing;
 import org.bouncycastle.openpgp.PGPPublicKeyRingCollection;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+import org.bouncycastle.util.encoders.Hex;
 
 /*
  * Hints:
@@ -63,17 +72,84 @@ public class OpenpgpUtil {
 	 *  - Public key algorithms: http://tools.ietf.org/html/rfc4880#section-9.1
 	 *  - Public key IDs and fingerprints: http://tools.ietf.org/html/rfc4880#section-12.2
 	 */
+	@SuppressWarnings("unchecked")
 	public static void listPublicKeyRing(PGPPublicKeyRing keyRing) {
-		// TODO: implement
+		// TODOdone: implement
 		// Hint: use String.format() to build strings with multiple variables.
+		
+		Iterator<?> keyIterator = keyRing.getPublicKeys();
+		while (keyIterator.hasNext()) {
+			PGPPublicKey pgpPublicKey = (PGPPublicKey) keyIterator.next();
+			
+			String keyType = pgpPublicKey.isMasterKey() ? "pub" : "sub";
+			int keyVersion = pgpPublicKey.getVersion();
+			int keyLength = pgpPublicKey.getBitStrength();
+			String keyAlgorithm = toAlgorithmString(pgpPublicKey.getAlgorithm());
+			
+			String validFrom = pgpPublicKey.getCreationTime().toString();
+			long validFromUnix = pgpPublicKey.getCreationTime().getTime() / 1000;
+			String validUntil = "forever";
+			long validUntilUnix = -1;
+			
+			int validDays = pgpPublicKey.getValidDays();
+			if (validDays > 0) {
+				Calendar tmp = Calendar.getInstance();
+				tmp.setTime(pgpPublicKey.getCreationTime());
+				tmp.add(Calendar.DAY_OF_YEAR, validDays);
+				
+				validUntil = tmp.getTime().toString();
+				validUntilUnix = tmp.getTimeInMillis() / 1000;
+			}
+			
+			String fingerprint = new String(Hex.encode(pgpPublicKey.getFingerprint()));
+			String fingerprintAlgorithm = "Undefined";
+			
+			if (keyVersion == 3) {
+				fingerprintAlgorithm = "MD5";
+			} else if (keyVersion == 4) {
+				fingerprintAlgorithm = "SHA-1";
+			}
+			
+			// Print key params
+			System.out.println(String.format("Key: %s v%d %d-bit %s", keyType, keyVersion, keyLength, keyAlgorithm));
+			System.out.println(String.format("ID: %S", Long.toHexString(pgpPublicKey.getKeyID())));
+			System.out.println(String.format("Valid from: %s (%d)", validFrom, validFromUnix));
+			System.out.println(String.format("Valid until: %s (%d)", validUntil, validUntilUnix));
+			
+			// Print key fingerprint
+			System.out.println(String.format("Key fingerprint: %s (%s)", fingerprint, fingerprintAlgorithm));
+			
+			// Print user user ids
+			Iterator<String> userIds = pgpPublicKey.getUserIDs();
+			while (userIds.hasNext()) {
+				String userId = (String) userIds.next();
+				System.out.println(String.format("User ID: %s", userId));
+			}
+			
+			// print signatures
+			Iterator<PGPSignature> signatures = pgpPublicKey.getSignatures();
+			while (signatures.hasNext()) {
+				PGPSignature signature = (PGPSignature) signatures.next();
+				String sig = Long.toHexString(signature.getKeyID());
+				System.out.println(String.format("Signed by: %S", sig));
+			}
+			
+			System.out.println();
+		}
 	}
 
 	/**
 	 * Prints this OpenPGP keyring collection contents to stdout (1 p).
 	 */
 	public static void listPublicKeyRingCollection(PGPPublicKeyRingCollection keyRingCollection) {
-		// TODO: implement
+		// TODOdone: implement
 		// Hint: use listPublicKeyRing(PGPPublicKeyRing) method.
+		
+		@SuppressWarnings("unchecked")
+		Iterator<PGPPublicKeyRing> rings = keyRingCollection.getKeyRings();
+		while (rings.hasNext()) {
+			listPublicKeyRing(rings.next());
+		}
 	}
 
 	/**
@@ -81,8 +157,11 @@ public class OpenpgpUtil {
 	 */
 	public static PGPPublicKeyRing readPublicKeyRing(String filePath)
 			throws IOException {
-		// TODO: implement
-		return null;
+		// TODOdone: implement
+		
+		FileInputStream in = new FileInputStream(filePath);
+		PGPPublicKeyRing ring = new PGPPublicKeyRing(in, new BcKeyFingerprintCalculator());
+		return ring;
 	}
 
 	/**
@@ -90,8 +169,11 @@ public class OpenpgpUtil {
 	 */
 	public static PGPPublicKeyRingCollection readPublicKeyRingCollection(String filePath)
 			throws IOException, PGPException {
-		// TODO: implement
-		return null;
+		// TODOdone: implement
+		
+		FileInputStream in = new FileInputStream(filePath);
+		PGPPublicKeyRingCollection coll = new PGPPublicKeyRingCollection(in);
+		return coll;
 	}
 
 	/**
@@ -100,6 +182,33 @@ public class OpenpgpUtil {
 	public static void writePublicKeyRingCollection(
 			PGPPublicKeyRingCollection keyRingCollection, String filePath)
 					throws IOException {
-		// TODO: implement
+		// TODOdone: implement
+		
+		FileOutputStream fos = new FileOutputStream(filePath);
+		fos.write(keyRingCollection.getEncoded());
+		fos.flush();
+		fos.close();
+	}
+	
+	private static String toAlgorithmString(int code) {
+		switch (code) {
+		case PublicKeyAlgorithmTags.RSA_GENERAL:
+		case PublicKeyAlgorithmTags.RSA_ENCRYPT:
+		case PublicKeyAlgorithmTags.RSA_SIGN:
+			return "RSA";
+		case PublicKeyAlgorithmTags.DSA:
+			return "DSA";
+		case PublicKeyAlgorithmTags.ELGAMAL_ENCRYPT:
+		case PublicKeyAlgorithmTags.ELGAMAL_GENERAL:
+			return "Elgamal";
+		case PublicKeyAlgorithmTags.DIFFIE_HELLMAN:
+			return "Diffie Hellman";
+		case PublicKeyAlgorithmTags.EC:
+			return "EC";
+		case PublicKeyAlgorithmTags.ECDSA:
+			return "ECDSA";
+		default:
+			return "Unknown";
+		}
 	}
 }
